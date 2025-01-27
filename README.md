b09315511ef28d04eadb97efb2e397c1e74538e1
afresquet <29437693+afresquet@users.noreply.github.com>
2025-01-27 13:43:24 +0100

    add rust toolchain template

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Nix Flake Templates
+
+To initialize a flake run the command:
+
+```sh
+nix flake init --template github:afresquet/nix-flake-templates#<template_name>
+```
+
+### Rust Devenv
+
+```sh
+nix flake init --template github:afresquet/nix-flake-templates#rust
+```
+
+### Rust Overlay for toolchain.toml files
+
+```sh
+nix flake init --template github:afresquet/nix-flake-templates#rust-toolchain
+```
--- flake.nix
+++ flake.nix
@@ -6,13 +6,28 @@
     {
       templates =
         let
+          welcomeText = ''
+            # `.direnv` and `.devenv` should be added to `.gitignore`
+            ```sh
+              echo .direnv >> .gitignore
+              echo .devenv >> .gitignore
+            ```
+          '';
+
           rust = {
             path = ./rust;
             description = "Devenv Rust environment";
+            inherit welcomeText;
           };
         in
         {
           inherit rust;
+
+          rust-toolchain = {
+            path = ./rust-toolchain;
+            description = "Rust Overlay toolchain.toml environment";
+            inherit welcomeText;
+          };
 
           default = rust;
         };
